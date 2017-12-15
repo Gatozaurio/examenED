93df28b1cc14eadfb4ab69ffb038afc54cbff7d3
Gatozaurio <32274582+Gatozaurio@users.noreply.github.com>
2017-12-15 18:23:14 +0100

    #2

--- src/modulos/ExamenED.java
+++ src/modulos/ExamenED.java
@@ -5,46 +5,48 @@ import java.util.Scanner;
 
 public class ExamenED {
 
-    static int w = 30;
+    static int maximoNumeroAleatorio = 30;
 
     public static void main(String args[]){
 
         System.out.println("INTRODUCE UN NUMERO: ");
         Scanner input = new Scanner(System.in);
-        int m = input.nextInt();
-        Random random = new Random();
-        int []a = new int[m];
-        for(int in=0; in<m; in++)
+        int numeroIntroducido = input.nextInt();        // Lee un número introducido y asigna ese número a una variable
+        Random numeroAleatorio = new Random();          // Crea un nuevo objeto de la clase formulario
+        int []arrayDeNumeros = new int[numeroIntroducido];  // Crea un array del tamaño del número leido por el escáner
+        for(int auxiliar=0; auxiliar<numeroIntroducido; auxiliar++)
         {
-            a[in] = random.nextInt(ExamenED.w);
+            arrayDeNumeros[auxiliar] = numeroAleatorio.nextInt(ExamenED.maximoNumeroAleatorio);     // Rellena el array con números aleatorios comprendidos entre 0 y 29
         }
 
-        ExamenED.ejecutarAlgoritmo(a);
+        ExamenED.ejecutarAlgoritmo(arrayDeNumeros);              // Ejecuta el método con el array de números aleatorios
+
+
         input.close();
     }
 
 
-    public static void ejecutarAlgoritmo(int[] a){
+    public static void ejecutarAlgoritmo(int[] arrayDeNumeros){
 
-        int x = 0;
-        int y = ExamenED.w;
+        int numeroMenor = 0;
+        int numeroMayor = ExamenED.maximoNumeroAleatorio;
 
-        for(int in=0; in<a.length; in++)
+        for(int auxiliar=0; auxiliar<arrayDeNumeros.length; auxiliar++)
         {
-            System.out.println(a[in]);
-            if(a[in] > x ){
-                x = a[in];
+            System.out.println(arrayDeNumeros[auxiliar]);   // Imprime el número que se encuentra en esa posición del Array
+            if(arrayDeNumeros[auxiliar] > numeroMenor ){              // Si el número que se encuentra en esa posición del array es mayor que numeroMenor...
+                numeroMenor = arrayDeNumeros[auxiliar];               // ...asigna el valor de ese número a numeroMenor
             }
 
-            if(a[in] < y){
-                y = a[in];
+            if(arrayDeNumeros[auxiliar] < numeroMayor){               // Si el número que se encuentra en esa posición del Array es menor que numeroMayor...
+                numeroMayor = arrayDeNumeros[auxiliar];               // ...asigna el valor de ese número a numeroMayor
             }
         }
 
-        int z = y *x;
-        if (z == 0 || (z % 2 != 0)  ){
+        int numerosMultiplicados = numeroMayor *numeroMenor;                                        // Asigna a numerosMultiplicados el valor de numeroMayor por numeroMenor
+        if (numerosMultiplicados == 0 || (numerosMultiplicados % 2 != 0)  ){
             System.out.println("NO ES");
-        }else{
+        }else{                                                                                      // Dice si numerosMultiplicados es par o no
             System.out.println("ES");
         }
 
